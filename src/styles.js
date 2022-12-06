fd46c0fb6a78fcb5881351ca6e016415f9b83928
Kaio Moura <kaioabrahaonepo@gmail.com>
2022-12-06 13:41:30 -0300

    Formatação dos estilos globais da aplicação

--- src/styles.js
+++ src/styles.js
@@ -4,4 +4,9 @@ export const Container = styled.div`
     height: 100vh;
     width: 100%;
     backgrond-color: #FAFAFA;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
+
