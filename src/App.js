f8c9a32c067b7742a57566b0de73f206cccd4289
Kaio Moura <kaioabrahaonepo@gmail.com>
2022-12-06 14:48:06 -0300

    Montagem do layout da calculadora

--- src/App.js
+++ src/App.js
@@ -8,17 +8,28 @@ const App = () => {
       <Content>
         <Input/>
         <Row>
-          <Button/>
-          <Button/>
-          <Button/>
-          <Button/>
+          <Button label="C"/>
+          <Button label="%"/>
+          <Button label="/"/>
+          <Button label="*"/>
         </Row>
-
         <Row>
-          <Button/>
-          <Button/>
-          <Button/>
-          <Button/>
+          <Button label="1"/>
+          <Button label="2"/>
+          <Button label="3"/>
+          <Button label="-"/>
+        </Row>
+        <Row>
+          <Button label="4"/>
+          <Button label="5"/>
+          <Button label="6"/>
+          <Button label="+"/>
+        </Row>
+        <Row>
+          <Button label="7"/>
+          <Button label="8"/>
+          <Button label="9"/>
+          <Button label="="/>
         </Row>
         
       </Content>
