f1916d5c43bc562f564857904611b81f02ad6322
Kaio Moura <kaioabrahaonepo@gmail.com>
2022-12-06 14:29:49 -0300

    Estilização inicial dos botões

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,1 +1,10 @@
 import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 10px;
+    border: 1px solid #CDCDCD;
+    background-color: #0000F0;
+    color: #FFFFFF;
+    font-size: 24px;
+    font-weight: 700;
+`
