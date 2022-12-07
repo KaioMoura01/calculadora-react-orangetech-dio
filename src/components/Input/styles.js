67bdeb69c24d11052631955b9fe1807948c130d3
Kaio Moura <kaioabrahaonepo@gmail.com>
2022-12-06 22:17:18 -0300

    Continuação dos testes no display

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -3,15 +3,16 @@ import styled from "styled-components";
 export const InputContainer = styled.div`
     width: 100%;
     height: 75px;
-    background-color: #AAAA;
+    background-color: #6495ED;
     display: flex;
+    justify-content: right;
 
     input{
         width: 100%;
         height: 75px;
         background-color: #6495ED;
         border: 0;
-        padding: 0 10px;
+        padding: 0;
         font-size: 24px;
         font-family: 'Roboto';
         color: #FFFFFF;
